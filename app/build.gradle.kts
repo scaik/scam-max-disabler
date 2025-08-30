80cfa2b8734c6f7144fa40176ba8511497a65212
Evsey Kazansky <67639168+scaik@users.noreply.github.com>
2025-08-31 00:30:15 +0300

    🔧 chore: bump app version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.scaik.scammaxdisabler"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.2"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
